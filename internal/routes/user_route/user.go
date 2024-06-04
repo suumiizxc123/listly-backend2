7a9ae279d9e7e7c8a64343201c3599bad284b6b3
suumiizxc <ch.sumyadorj@gmail.com>
2024-06-04 12:19:02 +0800

    update mmm

--- internal/routes/user_route/user.go
+++ internal/routes/user_route/user.go
@@ -12,7 +12,7 @@ func UserRoute(r *gin.RouterGroup) {
 	{
 		userGroup.POST("/register", middleware.CheckSecret(), user_handler.CreateUser)
 		userGroup.POST("/login", middleware.CheckSecret(), user_handler.LoginUser)
-		userGroup.GET("/list", middleware.AuthUser(), user_handler.GetUserList)
+		userGroup.GET("/list", user_handler.GetUserList)
 		userGroup.GET("/get", middleware.AuthUser(), user_handler.GetUser)
 		userGroup.PATCH("/update", middleware.AuthUser(), user_handler.UpdateUser)
 		userGroup.PATCH("/update-password", middleware.AuthUser(), user_handler.UpdateUserPassword)
